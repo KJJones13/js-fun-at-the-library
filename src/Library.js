47c90bcc93566a175ae53ab737c3f41dc1b6a844
Kyle Jones <rmbart395@yahoo.com>
2020-05-10 21:04:27 -0600

    First project

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,44 @@
+function createLibrary(name) {
+  var shelves = {fantasy: [], fiction: [], nonFiction: []};
+  var newLibrary = {name: name, shelves: shelves};
+    return newLibrary;
+}
 
+function addBook(library, book) {
+  var bookGenre = book.genre;
+    if (bookGenre == 'fantasy') {
+    library.shelves.fantasy.push(book);
+  } else if (bookGenre == 'nonFiction') {
+    library.shelves.nonFiction.push(book);
+  } else {
+    library.shelves.fiction.push(book);
+  }
+}
+
+function checkoutBook(library, name) {
+  for (var i = 0; i < library.shelves.fantasy.length; i++) {
+    if (library.shelves.fantasy[i].title == name) {
+      library.shelves.fantasy.splice(i, 1);
+      return "You have now checked out "+name+" from the "+library.name;
+    }
+  }
+  for (var i = 0; i < library.shelves.fiction.length; i++) {
+    if (library.shelves.fiction[i].title == name) {
+      library.shelves.fiction.splice(i, 1);
+      return "You have now checked out "+name+" from the "+library.name;
+    }
+  }
+  for (var i = 0; i < library.shelves.nonFiction.length; i++) {
+    if (library.shelves.nonFiction[i].title == name) {
+      library.shelves.nonFiction.splice(i, 1);
+      return "You have now checked out "+name+" from the "+library.name;
+    }
+  }
+  return "Sorry, there are currently no copies of "+name+" available at the "+library.name;
+}
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+createLibrary: createLibrary,
+addBook: addBook,
+checkoutBook: checkoutBook
 };
--- src/warm-up.js
+++ src/warm-up.js
@@ -11,24 +11,24 @@
 // a book's title and page count.
 
 // var title = "Harry Potter";
-// var ...
+// var pages = 100;
 
 
 
 // Reassign (not initialize) your "page" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
+// of what it currently is
 
-
+// pages = 75;
 
 // Initialize a new variable "movie", but leave it undefined
 
-
+// var movie = undefined;
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+// var sequel = "Harry Potter" + 2;
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
@@ -47,7 +47,7 @@
 
 
 // Express the following in code: If the variable title is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
 
 
